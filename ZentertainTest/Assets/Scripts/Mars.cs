6a7b316c33ebfff814a11a91484ba639d90baa0f
duke <duke@crenolab.com>
2015-06-25 19:29:12 +0800

    Finish Quiz2

---
+++ ZentertainTest/Assets/Scripts/Mars.cs
@@ -0,0 +1,171 @@
+﻿using System;
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Mars : MonoBehaviour {
+
+	public int width;
+	public int height;
+	public int[,] map;
+	public List<Rover> rovers = new List<Rover>();
+
+	public TextAsset configFile;
+	public GameObject floorPrefab;
+	public GameObject roverPrefab;
+
+	public bool HasObstacle(int _x, int _y)
+	{
+		if(IsOutOfBoundry(_x, _y))
+			return false;
+		return map[_x, _y] == 1;
+	}
+
+	public bool IsOutOfBoundry(int _x, int _y)
+	{
+		if(_x < 0 || _x >= width || _y < 0 || _y >= height)
+			return true;
+		return false;
+	}
+
+	// Use this for initialization
+	void Start () {
+		InitMars();
+		CreateFloors();
+//		Debug.Log(width);
+//		Debug.Log(height);
+//		Debug.Log(rovers.Count);
+	}
+	
+	// Update is called once per frame
+	void OnGUI () {
+		if(GUILayout.Button("NextStep"))
+		{
+			UpdateMars();
+		}
+	}
+
+	void InitMars()
+	{
+		Debug.Log(configFile.text);
+		
+		bool hasReadFirstLine = false;
+		bool hasNewRover = false;
+		foreach(var line in configFile.text.Split('\n'))
+		{
+//			Debug.Log("line:" + line);
+			string trimedLine = line.Trim();
+			if(!hasReadFirstLine)
+			{
+				width = int.Parse(trimedLine.Split(',')[0]);
+				height = int.Parse(trimedLine.Split(',')[1]);
+				map = new int[width, height];
+				for(int i = 0; i < width; ++i)
+					for(int j = 0; j < height; ++j)
+						map[i, j] = 0;
+				hasReadFirstLine = true;
+			}
+			else 
+			{
+				if(!hasNewRover)
+				{
+					hasNewRover = true;
+					string[] roverDetails = trimedLine.Split(',');
+					int x = int.Parse(roverDetails[0]);
+					int y = int.Parse(roverDetails[1]);
+					Rover.Direction dir = (Rover.Direction)Enum.Parse(typeof(Rover.Direction), roverDetails[2]);
+					
+					GameObject newRover = Instantiate(roverPrefab) as GameObject;
+					newRover.name = "Rover" + (rovers.Count+1);
+					Rover newRoverScript = newRover.GetComponent<Rover>();
+					newRoverScript.DrawRover(x, y, dir);
+					rovers.Add(newRoverScript);
+					map[x, y] = 1;
+				}
+				else
+				{
+					Rover.Command[] commands = new Rover.Command[trimedLine.Length];
+					for(int i = 0; i < trimedLine.Length; ++i)
+					{
+						//						Debug.Log(trimedLine[i]);
+						commands[i] = (Rover.Command)Enum.Parse(typeof(Rover.Command), trimedLine[i].ToString());
+					}
+					rovers[rovers.Count-1].SetRoverCommand(commands);
+					hasNewRover = false;
+				}
+			}
+		}
+		
+		if(hasNewRover)
+		{
+			Debug.LogError("Config file format error!");
+			return;
+		}
+	}
+
+	void CreateFloors()
+	{
+		GameObject floorGourp = new GameObject("floorGroup");
+		for(int i = 0; i < width; ++i)
+		{
+			for(int j = 0; j < height; ++j)
+			{
+				GameObject floor = Instantiate(floorPrefab, new Vector3(i, 0, j), Quaternion.identity) as GameObject;
+				floor.transform.parent = floorGourp.transform;
+			}
+		}
+	}
+
+	void UpdateMars()
+	{
+		for(int i = 0; i < rovers.Count; ++i)
+		{
+			Rover rover = rovers[i];
+			if(IsOutOfBoundry(rover.PosX, rover.PosY))
+				rover.Broken();
+			else
+			{
+				Rover.Command nowCommand = rover.PeekCommand();
+				switch(nowCommand)
+				{
+				case Rover.Command.M:
+					if ((rover.Dir == Rover.Direction.N && !HasObstacle(rover.PosX, rover.PosY+1)) ||
+					    (rover.Dir == Rover.Direction.E && !HasObstacle(rover.PosX+1, rover.PosY)) ||
+					    (rover.Dir == Rover.Direction.W && !HasObstacle(rover.PosX-1, rover.PosY)) ||
+					    (rover.Dir == Rover.Direction.S && !HasObstacle(rover.PosX, rover.PosY-1)))
+					{
+						map[rover.PosX, rover.PosY] = 0;
+						rover.DoMove();
+						if(!IsOutOfBoundry(rover.PosX, rover.PosY))
+							map[rover.PosX, rover.PosY] = 1;
+					}	
+					break;
+				case Rover.Command.L:
+					rover.DoTurnL();
+					break;
+				case Rover.Command.R:
+					rover.DoTurnR();
+					break;
+				case Rover.Command.B:
+					if ((rover.Dir == Rover.Direction.N && !HasObstacle(rover.PosX, rover.PosY-1)) ||
+					    (rover.Dir == Rover.Direction.E && !HasObstacle(rover.PosX-1, rover.PosY)) ||
+					    (rover.Dir == Rover.Direction.W && !HasObstacle(rover.PosX+1, rover.PosY)) ||
+					    (rover.Dir == Rover.Direction.S && !HasObstacle(rover.PosX, rover.PosY+1)))
+					{
+						map[rover.PosX, rover.PosY] = 0;
+						rover.DoBack();
+						if(!IsOutOfBoundry(rover.PosX, rover.PosY))
+							map[rover.PosX, rover.PosY] = 1;
+					}	
+					break;
+				case Rover.Command.NULL:
+					Debug.Log("No more command");
+					break;
+				}
+			}
+			rover.LogState();
+		}
+	}
+
+
+}
---
+++ ZentertainTest/Assets/Scripts/Rover.cs
@@ -0,0 +1,146 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Rover : MonoBehaviour {
+
+	public enum Direction
+	{
+		N=0,
+		E=1,
+		S=2,
+		W=3
+	}
+	public enum Command
+	{
+		NULL,
+		M,
+		L,
+		R,
+		B
+	}
+
+	public bool IsAlive
+	{
+		get; 
+		private set;
+	}
+
+	public int PosX
+	{
+		get;
+		private set;
+	}
+	public int PosY
+	{
+		get;
+		private set;
+	}
+	public Direction Dir
+	{
+		get;
+		private set;
+	}
+
+	public Command[] Commands
+	{
+		get;
+		private set;
+	}
+	public int CurrentActionIndex
+	{
+		get;
+		private set;
+	}
+	// Use this for initialization
+	void Start () {
+		IsAlive = true;
+		CurrentActionIndex = 0;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void DrawRover(int _x, int _y, Direction _dir)
+	{
+		PosX = _x;
+		PosY = _y;
+		Dir = _dir;
+		transform.position = new Vector3(PosX, transform.position.y, PosY);
+		if(Dir == Direction.N)
+			transform.localEulerAngles = Vector3.zero;
+		else if(Dir == Direction.E)
+			transform.localEulerAngles = new Vector3(0, 90, 0);
+		else if(Dir == Direction.W)
+			transform.localEulerAngles = new Vector3(0, -90, 0);
+		else if(Dir == Direction.S)
+			transform.localEulerAngles = new Vector3(0, 180, 0);
+	}
+
+	public void SetRoverCommand(Command[] _commands)
+	{
+		Commands = _commands;
+	}
+
+	public Command PeekCommand()
+	{
+		if(CurrentActionIndex >= Commands.Length)
+			return Command.NULL;
+		return Commands[CurrentActionIndex];
+	}
+
+
+
+	public void Broken()
+	{
+		Debug.Log("DEAD");
+		IsAlive = false;
+		return;
+	}
+
+	public void LogState()
+	{
+		Debug.Log(string.Format("{0}:[{1},{2},{3}],{4}", gameObject.name, PosX, PosY, Dir, IsAlive ? "Alive" : "Dead"));
+	}
+
+	public void DoMove()
+	{
+		transform.Translate(new Vector3(0, 0, 1), Space.Self);
+		PosX = (int)transform.position.x;
+		PosY = (int)transform.position.z;
+		SkipCommand();
+	}
+
+	public void DoTurnL()
+	{
+		transform.Rotate(new Vector3(0, -90, 0));
+		int DirInNum = (int)Dir;
+		DirInNum -= 1;
+		DirInNum = DirInNum < 0 ? DirInNum+4 : DirInNum;
+		Dir = (Direction)DirInNum;
+		SkipCommand();
+	}
+
+	public void DoTurnR()
+	{
+		transform.Rotate(new Vector3(0, 90, 0));
+		Dir = (Direction)(((int)Dir+1)%4);
+		SkipCommand();
+	}
+
+	public void DoBack()
+	{
+		transform.Rotate(new Vector3(0, 180, 0));
+		transform.Translate(new Vector3(0, 0, 1), Space.Self);
+		PosX = (int)transform.position.x;
+		PosY = (int)transform.position.z;
+		Dir = (Direction)(((int)Dir+2)%4);
+		SkipCommand();
+	}
+
+	void SkipCommand()
+	{
+		CurrentActionIndex++;
+	}
+}
